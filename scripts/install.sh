1d9eaf6790d7b76457796cdc8adaffd475688103
snjy5 <92902636+snjy5@users.noreply.github.com>
2026-08-09 15:34:27 +0530

    Last Sync: 2026-08-09 15:34 (Mobile)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,14 @@
 apt install ansible -y
 nms_dir="ansible/roles/nginx"
-req_file="$nms_dir/requirements.yml"
-nms_install_file="$nms_dir/nms-install.yml"
-nms_pb_file="$nms_dir/nms-playbook.yml"
+nms_req="$nms_dir/requirements.yml"
+nms_install="$nms_dir/nms-install.yml"
+nms_pb="$nms_dir/nms-playbook.yml"
+inventory="ansible/inventory/inventory.yml"
+pb="ansible/inventory/playbook.yml"
 
+ansible-playbook -i $inventory $pb
+ansible-galaxy install -fr $nms_req
+ansible-playbook -i $inventory $nms_install
+ansible-playbook -i $inventory $nms_pb
 
-ansible-galaxy install -fr $req_file
-ansible-playbook -i ansible/inventory $nms_install_file
-ansible-playbook -i ansible/inventory $nms_pb_file
-
-timeout 30 ansible all -i ansible/inventory/hosts.ini -m ping
+timeout 30 ansible all -i $inventory -m ping
