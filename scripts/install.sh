da622e05e5324274606c26a9c7847e4f96ff83e4
snjy5 <92902636+snjy5@users.noreply.github.com>
2026-08-09 10:36:52 +0530

    Last Sync: 2026-08-09 10:36 (Mobile)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 apt install ansible -y
-nms_dir="infrastructure/ansible/roles/nginx"
+nms_dir="ansible/roles/nginx"
 req_file="$nms_dir/requirements.yml"
 nms_install_file="$nms_dir/nms-install.yml"
 nms_pb_file="$nms_dir/nms-playbook.yml"
